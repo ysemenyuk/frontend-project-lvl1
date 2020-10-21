663fd6743c5b1c33b67a09013afa97cd5a301b74
ysemenyuk <invent@list.ru>
2020-10-21 08:25:49 +0300

    step4 v1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-read
+hexlet-frontend-projext-lvl-1
+<a href="https://codeclimate.com/github/ysemenyuk/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/a6d9e4e77083b09dc22d/maintainability" /></a>
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
+/* eslint-disable linebreak-style */
 import promptly from 'promptly';
+
 export default async () => {
-    const name = await promptly.prompt('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-  };
+  const name = await promptly.prompt('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
