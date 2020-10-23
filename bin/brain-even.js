b7a9d1ca2ed57797174ee153967ee3aa11652bca
ysemenyuk <invent@list.ru>
2020-10-23 05:46:48 +0300

    step5 v2 brain-even added

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
-import cli from '../src/cli.js';
+// import cli from '../src/cli.js';
 import isEven from '../src/brain-even-app.js';
 
 console.log('Welcome to the Brain Games!');
-cli();
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+// cli();
 isEven();
